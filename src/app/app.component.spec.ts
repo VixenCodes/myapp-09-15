815beb143e09529ad2fc43106465ee4fe3312da0
Anuntapome Sinsuak <taygo2546@gmail.com>
2024-08-08 17:29:02 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '1' title`, () => {
+  it(`should have the 'my-app01' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('1');
+    expect(app.title).toEqual('my-app01');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app01');
   });
 });
